14c3c65fd4535a8cd889a1780814211376531067
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-11-24 14:50:27 +0900

    validate wrong input

--- src/test/java/study/springrestapi/events/EventControllerTest.java
+++ src/test/java/study/springrestapi/events/EventControllerTest.java
@@ -98,4 +98,26 @@ public class EventControllerTest {
                 .andExpect(status().isBadRequest())
         ;
     }
+
+    @Test
+    void createEvent_Bad_Request_Wrong_Input() throws Exception {
+        EventDto eventDto = EventDto.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2023, 11, 22, 14, 23))
+                .closeEnrollmentDateTime(LocalDateTime.of(2023, 10, 23, 14, 23))
+                .beginEventDateTime(LocalDateTime.of(2023, 11, 24, 14, 23))
+                .endEventDateTime(LocalDateTime.of(2023, 11, 25, 14, 23))
+                .basePrice(10000)
+                .maxPrice(200)
+                .location("강남역 D2 스타텁 팩토리")
+                .build();
+
+        mockMvc.perform(post("/api/events")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(eventDto))
+                )
+                .andExpect(status().isBadRequest())
+        ;
+    }
 }
